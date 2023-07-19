1545c1ee04c9bd8895a0188986bf56d01b22613f
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-07-19 17:35:21 -0300

    Previniendo usuarios Duplicados

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -5,6 +5,15 @@ const usuarios = (req, res) => {
 }
 
 const registrar = async (req, res) => {
+    // Evitar registros duplicados
+    const {email} = req.body;
+    const existeUsuario = await Usuario.findOne({email:email});
+
+    if (existeUsuario) {
+        const error = new Error("Usuario ya registrado");
+        return res.status(400).json({msg:error.message});
+    }
+
     try {
         const usuario = new Usuario(req.body);
         const usuarioCreado = await usuario.save();
