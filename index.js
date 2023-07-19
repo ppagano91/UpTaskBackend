9b4492b07e9182f94ff1d93d649f5d7558fdf8eb
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-07-19 16:37:24 -0300

    Conectar App a la base de datos

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 // Para dependencias no necesito agregar la extensión .js
 import express from 'express';
+import dotenv from 'dotenv';
+import connectarDB from './config/db.js';
 
 // Para dependencias locales si necesito agregar la extensión .js
 
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+// Configurar dotenv
+dotenv.config();
+
+// Conectar a la base de datos
+connectarDB();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
     }
 );
