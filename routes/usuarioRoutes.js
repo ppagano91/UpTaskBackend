ffcf7c034cbf719b5e942f1f4fbe9b56430bef2d
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-07-19 17:04:36 -0300

    Probando verbos desde Postman

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -3,8 +3,19 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("Desde API/USUARIOS");
-}
-);
+    res.send("GET API/USUARIOS");
+});
+
+router.post("/", (req, res) => {
+    res.send("POST API/USUARIOS");
+});
+
+router.put("/", (req, res) => {
+    res.send("PUT API/USUARIOS");
+});
+
+router.delete("/", (req, res) => {
+    res.send("DELETE API/USUARIOS");
+});
 
 export default router;
