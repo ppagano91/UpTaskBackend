e0f14008bea468c37fd3706d21a01c9767afba4d
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-07-19 17:42:49 -0300

    Hashear los Passwords

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bycrypt from "bcrypt";
 
 const usuarioSchema = mongoose.Schema(
     {
@@ -31,6 +32,17 @@ const usuarioSchema = mongoose.Schema(
         timestamps: true,
     }    
 );
+
+// Hashear password
+usuarioSchema.pre("save", async function(next){
+    // Si el password ya está hasheado
+    if (!this.isModified("password")) {
+        // Continuar con el siguiente middleware
+        next();
+    }
+    const salt = await bycrypt.genSalt(10);
+    this.password = await bycrypt.hash(this.password, salt);
+});
 
 const Usuario = mongoose.model("Usuario", usuarioSchema);
 
