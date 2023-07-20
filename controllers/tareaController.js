537847d567ed0cce7b53faf8a59a8bb601d2094f
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-07-20 19:52:51 -0300

    Eliminar una tarea

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -85,7 +85,35 @@ const actualizarTarea = async (req, res) => {
   }
 };
 
-const eliminarTarea = async (req, res) => {};
+const eliminarTarea = async (req, res) => {
+  const { id } = req.params;
+  let tarea;
+
+  // Verificar si el id es válido
+  if (mongoose.Types.ObjectId.isValid(id)) {
+    // populate("proyecto") es para que me traiga el proyecto al que pertenece la tarea
+    tarea = await Tarea.findById(id).populate("proyecto");
+  }
+
+  if (!tarea) {
+    const error = new Error("Tarea no encontrada");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Acción no válida");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  try {
+    await tarea.deleteOne();
+    res.json({ msg: "Tarea Eliminada" });
+  } catch (error) {
+    console.log(error);
+    error = new Error("Hubo un error");
+    return res.status(500).json({ msg: error.message });
+  }
+};
 
 const cambiarEstadoTarea = async (req, res) => {};
 
