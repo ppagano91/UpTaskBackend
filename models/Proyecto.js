dbd172ec4a74881826b7f5df1c610562af368151
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-07-20 12:02:13 -0300

    Creando el Modelo de Proyecto

---
+++ models/Proyecto.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const ProyectoSchema = new mongoose.Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    descripcion: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    fechaEntrega: {
+      type: Date,
+      required: true,
+      default: Date.now(),
+    },
+    cliente: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    creador: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Usuario",
+    },
+    colaboradores: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Usuario",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Proyecto = mongoose.model("Proyecto", ProyectoSchema);
+
+export default Proyecto;
