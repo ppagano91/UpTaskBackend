97f5fec4772130f144436e3219d47184fb46940b
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-07-20 11:09:24 -0300

    Almacenando Nuevo Password

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,118 +1,139 @@
-import Usuario from '../models/usuario.js';
-import generarId from '../helpers/generar.js';
-import generarJWT from '../helpers/generarJWT.js';
+import Usuario from "../models/usuario.js";
+import generarId from "../helpers/generar.js";
+import generarJWT from "../helpers/generarJWT.js";
 
 const usuarios = async (req, res) => {
-    // Encotrar todos los usuarios
-    const usuarios = await Usuario.find();
-    res.json(usuarios);
-    
-}
+  // Encotrar todos los usuarios
+  const usuarios = await Usuario.find();
+  res.json(usuarios);
+};
 
 const registrar = async (req, res) => {
-    // Evitar registros duplicados
-    const {email} = req.body;
-    const existeUsuario = await Usuario.findOne({email:email});
+  // Evitar registros duplicados
+  const { email } = req.body;
+  const existeUsuario = await Usuario.findOne({ email: email });
 
-    if (existeUsuario) {
-        const error = new Error("Usuario ya registrado");
-        return res.status(400).json({msg:error.message});
-    }
+  if (existeUsuario) {
+    const error = new Error("Usuario ya registrado");
+    return res.status(400).json({ msg: error.message });
+  }
 
-    try {
-        const usuario = new Usuario(req.body);
-        usuario.token = generarId();
-        const usuarioCreado = await usuario.save();
-
-        console.log(usuario)
-        res.json(usuarioCreado);
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({msg:"Hubo un error"});
-        
-    }
-}
+  try {
+    const usuario = new Usuario(req.body);
+    usuario.token = generarId();
+    const usuarioCreado = await usuario.save();
+    res.json(usuarioCreado);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Hubo un error" });
+  }
+};
 
 const autenticar = async (req, res) => {
-    const {email, password} = req.body;
+  const { email, password } = req.body;
 
-    // Comprobar si el usuario existe
-    const usuario = await Usuario.findOne({email:email})
-    if(!usuario) {
-        const error = new Error("Usuario no registrado");
-        return res.status(400).json({msg:error.message});
-    }
-    console.log(usuario);
-    
-    // Comprobar si el usuario está confirmado
-    if(!usuario.confirmado) {
-        const error = new Error("Tu cuenta no ha sido confirmada");
-        return res.status(403).json({msg:error.message});
-    }
+  // Comprobar si el usuario existe
+  const usuario = await Usuario.findOne({ email: email });
+  if (!usuario) {
+    const error = new Error("Usuario no registrado");
+    return res.status(400).json({ msg: error.message });
+  }
 
-    // Comprobar si el password es correcto
-    if (await usuario.comprobarPassword(password)) {
-        res.json({_id:usuario._id,
-            nombre:usuario.nombre,
-            email:usuario.email,
-            token:generarJWT(usuario._id),           
-        });
-    }
-    else{
-        const error = new Error("Password incorrecto");
-        return res.status(400).json({msg:error.message});
-    }
-}
+  // Comprobar si el usuario está confirmado
+  if (!usuario.confirmado) {
+    const error = new Error("Tu cuenta no ha sido confirmada");
+    return res.status(403).json({ msg: error.message });
+  }
 
-const confirmar = async (req, res) => {    
-    const {token} = req.params;
-    const usuarioConfirmar = await Usuario.findOne({token:token});
-    if(!usuarioConfirmar) {
-        const error = new Error("Token no válido");
-        return res.status(403).json({msg:error.message});
-    }
+  // Comprobar si el password es correcto
+  if (await usuario.comprobarPassword(password)) {
+    res.json({
+      _id: usuario._id,
+      nombre: usuario.nombre,
+      email: usuario.email,
+      token: generarJWT(usuario._id),
+    });
+  } else {
+    const error = new Error("Password incorrecto");
+    return res.status(400).json({ msg: error.message });
+  }
+};
 
-    try {
-        usuarioConfirmar.confirmado = true;
-        usuarioConfirmar.token = "";
-        await usuarioConfirmar.save();
-        res.json({msg:"Usuario confirmado"});
-        
-    } catch (error) {
-        console.log(error);                
-    }    
-}
+const confirmar = async (req, res) => {
+  const { token } = req.params;
+  const usuarioConfirmar = await Usuario.findOne({ token: token });
+  if (!usuarioConfirmar) {
+    const error = new Error("Token no válido");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  try {
+    usuarioConfirmar.confirmado = true;
+    usuarioConfirmar.token = "";
+    await usuarioConfirmar.save();
+    res.json({ msg: "Usuario confirmado" });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const recuperarPassword = async (req, res) => {
-    const {email} = req.body;
+  const { email } = req.body;
 
-    const usuario = await Usuario.findOne({email:email})
-    if(!usuario) {
-        const error = new Error("Usuario no existe");
-        return res.status(400).json({msg:error.message});
-    }
+  const usuario = await Usuario.findOne({ email: email });
+  if (!usuario) {
+    const error = new Error("Usuario no existe");
+    return res.status(400).json({ msg: error.message });
+  }
 
-    try {
-        usuario.token = generarId();
-        await usuario.save();
-        res.json({msg:"Hemos enviado un email con las instrucciones para recuperar tu password"});
-        
-    } catch (error) {
-        console.log(error)        
-    }
-}
+  try {
+    usuario.token = generarId();
+    await usuario.save();
+    res.json({
+      msg: "Hemos enviado un email con las instrucciones para recuperar tu password",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const comprobarToken = async (req, res) => {
-    const {token} = req.params;
-    const tokenValido = await Usuario.findOne({token:token});
-    if(!tokenValido) {
-        const error = new Error("Token no válido");
-        return res.status(403).json({msg:error.message});
-    }
-    else{
-        res.json({msg:"Token válido y el Usuario existe"});
-    }
-}
+  const { token } = req.params;
+  const tokenValido = await Usuario.findOne({ token: token });
+  if (!tokenValido) {
+    const error = new Error("Token no válido");
+    return res.status(403).json({ msg: error.message });
+  } else {
+    res.json({ msg: "Token válido y el Usuario existe" });
+  }
+};
 
-export {usuarios, registrar, autenticar,confirmar,recuperarPassword, comprobarToken};
+const nuevoPassword = async (req, res) => {
+  const { token } = req.params;
+  const { password } = req.body;
+
+  const usuario = await Usuario.findOne({ token: token });
+  if (!usuario) {
+    const error = new Error("Usuario no válido");
+    return res.status(403).json({ msg: error.message });
+  } else {
+    usuario.password = password;
+    usuario.token = "";
+    try {
+      await usuario.save();
+      res.json({ msg: "Password actualizado" });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+export {
+  usuarios,
+  registrar,
+  autenticar,
+  confirmar,
+  recuperarPassword,
+  comprobarToken,
+  nuevoPassword,
+};
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,8 +1,15 @@
 import express from "express";
-import {usuarios, registrar, autenticar,confirmar, recuperarPassword, comprobarToken} from "../controllers/usuarioController.js";
+import {
+  usuarios,
+  registrar,
+  autenticar,
+  confirmar,
+  recuperarPassword,
+  comprobarToken,
+  nuevoPassword,
+} from "../controllers/usuarioController.js";
 
 const router = express.Router();
-
 
 // Autenticación, Registro y Confirmación de Usuarios
 
@@ -10,16 +17,20 @@ router.get("/", usuarios);
 router.post("/", registrar);
 router.post("/login", autenticar);
 router.get("/confirmar/:token", confirmar);
-router.post("/recuperar-password",recuperarPassword);
-router.get("/recuperar-password/:token",comprobarToken);
-
+router.post("/recuperar-password", recuperarPassword);
+/*
+Otra forma de hacerlo:
+router.route("/recuperar-password/:token").get(comprobarToken).post(nuevoPassword);
+*/
+router.get("/recuperar-password/:token", comprobarToken);
+router.post("/recuperar-password/:token", nuevoPassword);
 
 router.put("/", (req, res) => {
-    res.send("PUT API/USUARIOS");
+  res.send("PUT API/USUARIOS");
 });
 
 router.delete("/", (req, res) => {
-    res.send("DELETE API/USUARIOS");
+  res.send("DELETE API/USUARIOS");
 });
 
 export default router;
